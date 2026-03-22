d0182528dc474137caf985679ca1d4e9ab53f30f
VictorKhal <victorworld2305@gmail.com>
2026-03-23 02:03:59 +0300

    Change conf

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    headless: false,
+    // headless: false,
+    headless: !!process.env.CI,
   },
 
   projects: [
